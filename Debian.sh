628f29fe96c9149d17f1da59502bfbdfb67b3d3e
Swrich <64821916+Swrich@users.noreply.github.com>
2023-05-23 22:54:20 +0800

    Update Debian.sh

--- Debian.sh
+++ Debian.sh
@@ -1,4 +1,3 @@
-sudo apt-get remove docker docker-engine docker.io containerd runc -y && \
 sudo apt-get update -y && \
 sudo apt-get install ca-certificates curl gnupg && \
 sudo install -m 0755 -d /etc/apt/keyrings && \
